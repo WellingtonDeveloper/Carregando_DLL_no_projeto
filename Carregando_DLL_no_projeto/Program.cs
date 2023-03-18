e67624497c31cc122f7e62733ee70898b769171a
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2023-03-18 18:36:14 -0300

    Add project files.

---
+++ Carregando_DLL_no_projeto/Program.cs
@@ -0,0 +1,20 @@
+﻿using Criando_um_projeto_DLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carregando_DLL_no_projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A soma de 5 e 6 é:" + Matematica.soma(5, 6));
+            Console.WriteLine("A Multiplicação de 5 e 6 é:" + Matematica.Multiplica(5, 6));
+
+            Console.ReadKey();
+        }
+    }
+}
